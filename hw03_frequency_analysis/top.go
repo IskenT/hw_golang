04f99c0e8b2c723ba596de29860d771519bc9e06
IskenT <turanov2901@gmail.com>
2023-08-11 14:49:58 +0600

    test runned

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,44 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type wordCount struct {
+	word  string
+	count int
+}
+
+var wordCounts []wordCount
+
+func Top10(input string) []string {
+	splitedWords := strings.Fields(input)
+	// Count requency with dictionary
+	wordFreq := make(map[string]int)
+	for _, word := range splitedWords {
+		if _, exist := wordFreq[word]; exist {
+			wordFreq[word]++
+		} else {
+			wordFreq[word] = 1
+		}
+	}
+
+	// Sort by frequency
+	for word, count := range wordFreq {
+		wordCounts = append(wordCounts, wordCount{word, count})
+	}
+	sort.Slice(wordCounts, func(i, j int) bool {
+		if wordCounts[i].count == wordCounts[j].count {
+			return wordCounts[i].word < wordCounts[j].word
+		}
+		return wordCounts[i].count > wordCounts[j].count
+	})
+
+	// Получить топ 10 слов
+	var topWords []string
+	for i := 0; i < len(wordCounts) && i < 10; i++ {
+		topWords = append(topWords, wordCounts[i].word)
+	}
+	return topWords
 }

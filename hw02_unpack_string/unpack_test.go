fc8c65890bf87042a1554e90e45bd984ee8c4566
IskenT <turanov2901@gmail.com>
2023-08-12 21:24:12 +0600

    hw02 changed

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	test "github.com/stretchr/testify/require"
 )
 
 func TestUnpack(t *testing.T) {
@@ -16,30 +16,38 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
-		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: "OchkiN5ada", expected: "OchkiNNNNNada"},
+		{input: "di3mo4n", expected: "diiimoooon"},
+		{input: "ladas2eda2n", expected: "ladassedaan"},
+		{input: "belyash0", expected: "belyas"},
 	}
 
 	for _, tc := range tests {
 		tc := tc
 		t.Run(tc.input, func(t *testing.T) {
 			result, err := Unpack(tc.input)
-			require.NoError(t, err)
-			require.Equal(t, tc.expected, result)
+			test.NoError(t, err)
+			test.Equal(t, tc.expected, result)
 		})
 	}
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{
+		"3abc",
+		"45",
+		"aaa10b",
+		"chukcha11",
+		"belyash01",
+		"---10",
+		"*****!",
+		"aaa1**",
+	}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
 			_, err := Unpack(tc)
-			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
+			test.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
 		})
 	}
 }
